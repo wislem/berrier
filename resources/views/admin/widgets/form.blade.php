5bd3ac50b1257f091d06efe0fc7756289d132cb6
Peter <wislem@users.noreply.github.com>
2016-04-06 14:35:34 +0300

    Changed to adamwathan/bootform

--- resources/views/admin/widgets/form.blade.php
+++ resources/views/admin/widgets/form.blade.php
@@ -1,27 +1,29 @@
-{!! BootForm::open(['model' => $widget, 'store' => '\Wislem\Berrier\Http\Controllers\Modules\WidgetController@store', 'update' => '\Wislem\Berrier\Http\Controllers\Modules\WidgetController@update']) !!}
+@if($widget)
+    {!! BootForm::open()->action('/admin/widgets/' . $widget->id)->put() !!}
+    {!! BootForm::bind($widget) !!}
+@else
+    {!! BootForm::open()->action('/admin/widgets') !!}
+@endif
 <div class="box-body">
 
-    {!! BootForm::text('title', 'Title', null, ['id' => 'title']) !!}
+    {!! BootForm::text('title', 'Title') !!}
+    {!! BootForm::textarea('content', 'Content')->addClass('codemirror') !!}
+    {!! BootForm::text('path', 'Path to widget script (blade style)')->placeholder('E.g. widgets.my_widget') !!}
+    @if($widget)
+        {!! BootForm::select('pages[]', 'Appear in Pages', ['' => '---'] + $pages)->select($widget->pages()->lists('id')->toArray())->addClass('select2') !!}
+    @else
+        {!! BootForm::select('pages[]', 'Appear in Pages', ['' => '---'] + $pages)->addClass('select2') !!}
+    @endif
 
-    {!! BootForm::textarea('content', 'Content', null, ['id' => 'content', 'class' => 'codemirror']) !!}
-
-    {!! BootForm::text('path', 'Path to widget script (blade style)', null, ['id' => 'path', 'placeholder' => 'E.g. widgets.my_widget']) !!}
-
-    {!! BootForm::select('pages[]', 'Appear in Pages', $pages, ($widget) ? $widget->pages()->lists('id')->toArray() : null, ['id' => 'pages[]', 'multiple' => '', 'class' => 'select2']) !!}
-
-    {!! BootForm::select('position', 'Position', ['' => '---'] + config('berrier.theme.widget_positions.logo'), null, ['id' => 'position', 'class' => 'select2']) !!}
-
-    {!! BootForm::text('ordr', 'Order of appearance', null, ['id' => 'ordr']) !!}
-
-    {!! BootForm::checkbox('is_active', 'Visible', '1') !!}
-
-    {!! BootForm::checkbox('is_global', 'Global', '1') !!}
-
+    {!! BootForm::select('position', 'Position', ['' => '---'] + config('berrier.theme.widget_positions'))->addClass('select2') !!}
+    {!! BootForm::text('ordr', 'Order of appearance') !!}
+    {!! BootForm::checkbox('is_active', 'Visible')->defaultToChecked() !!}
+    {!! BootForm::checkbox('is_global', 'Global')->defaultToChecked() !!}
 </div>
 
 <div class="box-footer">
     <ul class="list-inline list-unstyled">
-        <li>{!! BootForm::submit('Save', ['class' => 'btn btn-primary']) !!}</li>
+        <li>{!! BootForm::submit('Save', 'btn btn-primary') !!}</li>
         <li><a href="{{URL::previous()}}" class="btn btn-default">Cancel</a></li>
     </ul>
 </div>
