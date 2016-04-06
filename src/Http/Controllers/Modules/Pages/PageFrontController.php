e5586ee959f969b180162da3f8ef6ccb0c307711
Peter <wislem@users.noreply.github.com>
2016-04-06 14:46:50 +0300

    Changed $page to $object

--- src/Http/Controllers/Modules/Pages/PageFrontController.php
+++ src/Http/Controllers/Modules/Pages/PageFrontController.php
@@ -8,41 +8,41 @@ use Wislem\Berrier\Models\Page;
 class PageFrontController extends Controller
 {
 
-    protected $page;
+    protected $object;
 
-    public function __construct(Page $page)
+    public function __construct(Page $object)
     {
-        $this->page = $page;
+        $this->object = $object;
     }
 
     public function index($slug)
     {
-        $page = $this->page->join('page_translations as t', 't.page_id', '=', 'pages.id')
-            ->select('pages.id', 'slug', 'title', 'content', 'meta_desc')->active()->whereSlug($slug)->first();
+        $object = $this->object->join('page_translations as t', 't.page_id', '=', 'pages.id')
+            ->select('id', 'slug', 'title', 'content', 'meta_desc')->active()->whereSlug($slug)->first();
 
-        if(!$page) {
+        if(!$object) {
             App::abort(404);
         }
 
         $meta = [
-            'title' => $page->title,
-            'desc' => $page->meta_desc
+            'title' => $object->title,
+            'desc' => $object->meta_desc
         ];
 
-        $page->content = str_replace('{{url}}', \Request::url(), $page->content);
-        $widgets = Widget::extract_unit($page->content, '{{', '}}');
+        $object->content = str_replace('{{url}}', \Request::url(), $object->content);
+        $widgets = Widget::extract_unit($object->content, '{{', '}}');
         foreach($widgets as $bbcode => $widget) {
-            $page->content = str_replace('{{' . $bbcode . '}}', $widget, $page->content);
+            $object->content = str_replace('{{' . $bbcode . '}}', $widget, $page->content);
         }
 
         if(\View::exists('berrier::themes.' . config('berrier.theme.name') . '.custom.' . $slug)) {
             return view('berrier::themes.' . config('berrier.theme.name') . '.custom.' . $slug)
-                ->with(compact('page'))
+                ->with(compact('object'))
                 ->with(compact('meta'));
         }
 
         return view('berrier::themes.' . config('berrier.theme.name') . '.page')
-            ->with(compact('page'))
+            ->with(compact('object'))
             ->with(compact('meta'));
     }
 
